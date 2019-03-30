293a964188635b5979dd02488562a2b0e4ee94a1
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-03-30 21:13:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,9 +4,9 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("هنا اي دي الروم")
+let channel =     client.channels.get("561611876769726476")
 setInterval(function() {
-channel.send(`هنا كلام ال سبام`);
+channel.send(`Mixwell Community ||Mohamad_Doleh|| Mixwell Community ||Mohamad_Doleh|| Mixwell Community ||Mohamad_Doleh|| Mixwell Community ||Mohamad_Doleh|| Mixwell Community ||Mohamad_Doleh|| Mixwell Community ||Mohamad_Doleh|| Mixwell Community ||Mohamad_Doleh|| `);
 }, 30)
 })
 
